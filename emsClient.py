305bed69fd005bb58f32aa5e96fbbd95ecdc705d
James <57487444+jamesmsnsixth@users.noreply.github.com>
2020-03-07 20:36:14 +0000

    Added comments

--- emsClient.py
+++ emsClient.py
@@ -1,3 +1,4 @@
+#Imports
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -11,51 +12,54 @@ from kivy.graphics import Color
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.floatlayout import FloatLayout
-##from plyer import gps
 
 
+#Main screen - pass in screen and widget module from Kivy
 class MainScreen(Screen, Widget):
     pass
 
-class messageOrganisersScreen(Screen, Widget):
+#Message organisers screen - pass in screen and widget module from Kivy
+    #Take input from user and set as msgInput
     msgInput = ObjectProperty(None)
+    #Function for sending message
     def sendMsg(self):
+        #For development: print message to screen
         print("Your message was: ", self.msgInput.text)
         self.msgInput.text = ""
 
+#Send location screen - pass in screen and widget module from Kivy
 class sendLocationScreen(Screen, Widget):
+    #Function to send location
     def sendManualLocation(self):
+        #For development: print a generic message to screen
         print("Your location will be sent.")
-    ##def sendLocation(self):
-##      gps.configure(on_location=self.on_gps_location)
-##      gps.start()
-##
-##    def on_gps_location(self, **kwargs):
-##        kwargs['lat']
-##        kwargs['lon']
-##        print(kwargs)
 
+#Inbox screen
 class inboxScreen(Screen, Widget):
     pass
 
+#Distress signal screen
 class distressScreen(Screen,Widget):
+    #Class for sending a distress signal
     def sendDistress(self):
+        #For development: print a generic message to screen
         print("Distress signal will be sent")
     
-
+#Set this class as the screen manager (ability to switch between multiple screens)
 class ScreenManagement(ScreenManager, Widget):
     pass
 
 
-   
+#Load emsclient.kv as styling file
 presentation = Builder.load_file("emsclient.kv")
 
 
+#Build the app
 class emsClient(App):
     def build(self):
         return presentation
 
-
+#Run the app
 if __name__ == "__main__":
     emsClient().run()
 
