8a5c07fe5a224f046bcf93acc9ec0f535f5bdd96
James <57487444+jamesmsnsixth@users.noreply.github.com>
2020-03-08 11:39:12 +0000

    Beautified code.
    Had a clean up: added comments, indentations and sorted out spacing.

--- emsClient.py
+++ emsClient.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -14,52 +14,61 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.floatlayout import FloatLayout
 
 
-#Main screen - pass in screen and widget module from Kivy
+# Main screen - pass in screen and widget module from Kivy
 class MainScreen(Screen, Widget):
     pass
 
-#Message organisers screen - pass in screen and widget module from Kivy
-    #Take input from user and set as msgInput
+# Message organisers screen - pass in screen and widget module from Kivy
+    # Take input from user and set as msgInput
     msgInput = ObjectProperty(None)
-    #Function for sending message
+    # Function for sending message
+
     def sendMsg(self):
-        #For development: print message to screen
+        # For development: print message to screen
         print("Your message was: ", self.msgInput.text)
         self.msgInput.text = ""
 
-#Send location screen - pass in screen and widget module from Kivy
+# Send location screen - pass in screen and widget module from Kivy
+
+
 class sendLocationScreen(Screen, Widget):
-    #Function to send location
+    # Function to send location
     def sendManualLocation(self):
-        #For development: print a generic message to screen
+        # For development: print a generic message to screen
         print("Your location will be sent.")
 
-#Inbox screen
+# Inbox screen
+
+
 class inboxScreen(Screen, Widget):
     pass
 
-#Distress signal screen
-class distressScreen(Screen,Widget):
-    #Class for sending a distress signal
+# Distress signal screen
+
+
+class distressScreen(Screen, Widget):
+    # Class for sending a distress signal
     def sendDistress(self):
-        #For development: print a generic message to screen
+        # For development: print a generic message to screen
         print("Distress signal will be sent")
-    
-#Set this class as the screen manager (ability to switch between multiple screens)
+
+# Set this class as the screen manager (ability to switch between multiple screens)
+
+
 class ScreenManagement(ScreenManager, Widget):
     pass
 
 
-#Load emsclient.kv as styling file
+# Load emsclient.kv as styling file
 presentation = Builder.load_file("emsclient.kv")
 
 
-#Build the app
+# Build the app
 class emsClient(App):
     def build(self):
         return presentation
 
-#Run the app
+
+# Run the app
 if __name__ == "__main__":
     emsClient().run()
-
